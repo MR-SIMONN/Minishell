2aad33689400f8647676d8113e2e55451354e59f
Mohamed El haia <moel-hai@e3r5p6.1337.ma>
2025-05-18 18:44:43 +0100

    everything is done except for the expending

--- Minishell/utils/ft_substr.c
+++ Minishell/utils/ft_substr.c
@@ -6,37 +6,29 @@
 /*   By: moel-hai <moel-hai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 17:30:32 by ielouarr          #+#    #+#             */
-/*   Updated: 2025/05/12 01:46:49 by moel-hai         ###   ########.fr       */
+/*   Updated: 2025/05/18 17:18:03 by moel-hai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Minishell.h"
 
-char	*ft_substr(char *s, unsigned int start, size_t len, t_data *data)
+char	*ft_substr(char *s, unsigned int start, size_t len, t_data *d)
 {
-	char	*sub;
-	size_t	i;
+	size_t			i;
+	char			*str;
 
 	if (!s)
 		return (NULL);
-	if (start >= (unsigned int)(ft_strlen(s)))
+	if (start >= ft_strlen (s))
 	{
-		sub = ft_malloc(1, data);
-		if (sub)
-			sub[0] = '\0';
-		return (sub);
+		str = ft_malloc (1, d);
+		return (*str = '\0', str);
 	}
-	if (start + len > ft_strlen(s))
-		len = ft_strlen(s) - start;
-	sub = ft_malloc(len + 1, data);
-	if (!sub)
-		return (NULL);
+	if (len > ft_strlen (s + start))
+		len = ft_strlen (s + start);
 	i = 0;
+	str = ft_malloc (len + 1, d);
 	while (i < len)
-	{
-		sub[i] = s[start + i];
-		i++;
-	}
-	sub[i] = '\0';
-	return (sub);
+		str[i++] = s[start++];
+	return (str[i] = '\0', str);
 }
